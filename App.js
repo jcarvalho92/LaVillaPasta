e01f3a86b9ddde639caef952eff91bf91a8ca25d
Juliana <julianadecarvalho92@gmail.com>
2021-02-18 14:02:06 -0500

    Add new Components and Login Screen
    - Add new Components (Screen, Text, TextInput, Form, FormField, ErrorMessage, submitButton).
    - Added login Screen
    - Using formik and Yup(validation).

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { StyleSheet} from 'react-native';
+import LoginScreen from './app/screens/LoginScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
   return (
-    <WelcomeScreen /> 
+    <LoginScreen /> 
   );
 }
 
